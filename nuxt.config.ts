6c7a6ea6e42e5d6b766c70f3ef8c86480b4cc4fb
serj2626 <serj2626@mail.ru>
2025-06-17 12:00:58 +0300

    edit settings in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  modules: ["nuxt-icon"],
+  css: ["@/assets/scss/global.scss"],
+  vite: {
+    vue: {
+      script: {
+        defineModel: true,
+        propsDestructure: true,
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+              @use '@/assets/scss/_vars.scss' as *;
+              @use '@/assets/scss/_mixins.scss' as *;
+              @use '@/assets/scss/_animations.scss' as *;
+            `,
+        },
+      },
+    },
+  },
+});
